3389eaa5436eac68407eb8678b12c8dfe5badca9
Gabriel Felipe <email@gabrielfelipe.com>
2021-06-24 19:31:40 -0300

    Removes unecessary lines

--- TP2-Albornoz-Chiminelli.rkt
+++ TP2-Albornoz-Chiminelli.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname TP2-Apellido1-Apellido2-Apellido3) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname TP2-Albornoz-Chiminelli) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 #|
 Trabajo Práctico 2: Listas
 Integrantes:
